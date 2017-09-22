ea99aadb3bb72282dd3b1a14283d80adb88893ba
Sam Galizia <samgalizia@gmail.com>
2017-09-22 10:03:25 -0700

    Completed Stochastic Sampling
    Added small test file to test the accuracy of the sampling method.
    The sampling is accurate within 1% of what would be expected.

--- sample.py
+++ sample.py
@@ -1,7 +1,8 @@
 #!python3
 
+from bisect import bisect_left
+from random import randint, random
 from sys import argv
-from random import randint
 import time
 
 
@@ -10,22 +11,78 @@ def create_histogram(filename):
     lines = f.readlines()
     f.close()
 
-    histogram = []
+    histogram = {}
     for line in lines:
         line = line.rstrip('\n').split(' ')
-        histogram.append([line[0], int(line[1])])
+        histogram[line[0]] = int(line[1])
     return histogram
+
+
+def create_relative_probabilities(hist):
+    total_words = get_total_word_count(hist)
+    for word in hist.keys():
+        freq = hist[word] / float(total_words)
+        hist[word] = freq
+    return hist
+
+
+def get_random_word(hist):
+    prob = random()
+    accumulator = 0.0
+    keys = list(hist.keys())
+    current_key = None
+
+    while accumulator < prob:
+        current_key = keys.pop(randint(0, len(keys) - 1))
+        if accumulator + hist[current_key] > prob:
+            break
+        else:
+            accumulator += hist[current_key]
+    return current_key
+
+
+def get_total_word_count(dic):
+    total_words = 0
+    for word in dic.keys():
+        total_words += dic[word]
+    return total_words
+
+
+def generate_sentence(data, length):
+    graph = create_histogram(data)
+    relative_freq = create_relative_probabilities(graph)
+    output = ""
+
+    for num in range(0, length):
+        new_word = get_random_word(relative_freq)
+        output += '{} '.format(new_word)
+    return output
+
+
+def test_frequency_distribution(data, length):
+    graph = create_histogram(data)
+    relative_freq = create_relative_probabilities(graph)
+    print(relative_freq)
+    results = {}
+
+    for num in range(0, length):
+        new_word = get_random_word(relative_freq)
+        if new_word in results:
+            results[new_word] += 1
+        else:
+            results[new_word] = 1
+    return results
 
 
 if __name__ == '__main__':
     start = time.time()
     arguments = argv[1:]
-    graph = create_histogram(arguments[0])
-    output = ""
 
-    for num in range(0, int(arguments[1])):
-        rand_idx = randint(0, len(graph) - 1)
-        output += (graph[rand_idx][0] + ' ')
+    print(generate_sentence(arguments[0], int(arguments[1])))
 
-    print(output)
+    # Uncomment this line if you want to test the accuracy of the sampling
+    # result = test_frequency_distribution(arguments[0], int(arguments[1]))
+    # for word in result.keys():
+    #     print('{0}: {1}'.format(word, result[word]))
+
     print('Time to execute: {0} seconds'.format(time.time() - start))
