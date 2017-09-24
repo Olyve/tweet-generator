1464bdb036e62bafb24cee69791e3ec77575f5e5
Sam Galizia <samgalizia@gmail.com>
2017-09-24 11:37:18 -0700

    Further Refined Corpus & Started Template Work
    Cleaned up the corpus, condensed the regex pattern for identifying
    the correct lines. Also started laying the ground work for using
    templates with Jinja2 to make the home page look better.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!python3
 
 from random import randint
-from flask import Flask
+from flask import Flask, render_template
 from scripts import sample
 
 app = Flask(__name__)
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return sample.generate_sentence('test_graph_data.txt', randint(6, 15))
+    quote = sample.generate_sentence('test_graph_data.txt', randint(6, 15))
+    return render_template('index.html', quote=quote)
 
 
 if __name__ == "__main__":
