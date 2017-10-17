1014de95d98d164003fb9a618e15533511ec0555
Sam Galizia <samgalizia@gmail.com>
2017-10-17 13:00:38 -0700

    Updated app to generate sentence for tweet

--- app.py
+++ app.py
@@ -13,8 +13,12 @@ def index():
     _file.close()
 
     markov = Markov(lines)
-    quote = markov.generate_sentence()
-    return render_template('index.html', quote=quote)
+    min_chars, max_chars = 25, 140
+    sentence = markov.generate_sentence()
+    while len(sentence) < min_chars or len(sentence) > max_chars:
+        # print('sentence too short/long, trying again...')
+        sentence = markov.generate_sentence()
+    return render_template('index.html', quote=sentence)
 
 
 if __name__ == "__main__":
